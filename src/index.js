0b7bd24fa1922a0f07699179bdc231e1998a467a
avi5079 <aviral.srivastava2102@gmail.com>
2023-12-14 00:12:33 +0530

    expensify_reducer

--- src/index.js
+++ src/index.js
@@ -10,29 +10,109 @@
 // );
 
 import { legacy_createStore as createStore, combineReducers } from "redux";
+import { v4 as uuidv4 } from "uuid";
 
-const demoState = {
-  expenses: [
-    {
-      id: "21fw",
-      description: "January Rent",
-      note: "This was the final payment",
-      amount: 54400,
-      createdAt: 0,
+// const demoState = {
+//   expenses: [
+//     {
+//       id: "21fw",
+//       description: "January Rent",
+//       note: "This was the final payment",
+//       amount: 54400,
+//       createdAt: 0,
+//     },
+//   ],
+//   filters: {
+//     text: "rent",
+//     sortBy: "date", // 'date' or 'amount'
+//     startDate: undefined,
+//     endDate: undefined,
+//   },
+// };
+
+const addExpense = ({
+  description = "",
+  note = "",
+  amount = 0,
+  createdAt = 0,
+} = {}) => {
+  return {
+    type: "ADD_EXPENSE",
+    expense: {
+      id: uuidv4(),
+      description: description,
+      note: note,
+      amount: amount,
+      createdAt: createdAt,
     },
-  ],
-  filters: {
-    text: "rent",
-    sortBy: "date", // 'date' or 'amount'
-    startDate: undefined,
-    endDate: undefined,
-  },
+  };
+};
+
+const removeExpense = ({ id } = {}) => {
+  return {
+    type: "REMOVE_EXPENSE",
+    id: id,
+  };
+};
+
+const editExpense = ({ id, updates }) => {
+  return {
+    type: "EDIT_EXPENSE",
+    id: id,
+    updates: updates,
+  };
+};
+
+const setTextFilter = ({ text = "" } = {}) => {
+  return {
+    type: "SET_TEXT_FILTER",
+    text,
+  };
+};
+
+const sortByAmount = () => {
+  return {
+    type: "SORT_BY_AMOUNT",
+  };
+};
+
+const sortByDate = () => {
+  return {
+    type: "SORT_BY_DATE",
+  };
+};
+
+const setStartDate = (startDate) => {
+  return {
+    type: "SET_START_DATE",
+    startDate,
+  };
+};
+
+const setEndDate = (endDate) => {
+  return {
+    type: "SET_END_DATE",
+    endDate,
+  };
 };
 
 const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    case "REMOVE_EXPENSE":
+      return state.filter((expense) => expense.id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        }
+      });
     default:
       return state;
   }
@@ -47,6 +127,31 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case "SET_TEXT_FILTER":
+      return {
+        ...state,
+        text: action.text,
+      };
+    case "SORT_BY_AMOUNT":
+      return {
+        ...state,
+        sortBy: "amount",
+      };
+    case "SORT_BY_DATE":
+      return {
+        ...state,
+        sortBy: "date",
+      };
+    case "SET_START_DATE":
+      return {
+        ...state,
+        startDate: action.startDate,
+      };
+    case "SET_END_DATE":
+      return {
+        ...state,
+        endDate: action.endDate,
+      };
     default:
       return state;
   }
@@ -59,4 +164,26 @@ const store = createStore(
   })
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const expense1 = store.dispatch(
+  addExpense({ description: "Rent", amount: 100 })
+);
+// console.log(expense1);
+store.dispatch(addExpense({ description: "Coffee", amount: 50 }));
+store.dispatch(removeExpense({ id: expense1.expense.id }));
+store.dispatch(
+  editExpense({ id: expense1.expense.id, updates: { amount: 500 } })
+);
+store.dispatch(setTextFilter({ text: "Rent" }));
+store.dispatch(setTextFilter());
+
+store.dispatch(sortByAmount());
+store.dispatch(sortByDate());
+
+store.dispatch(setStartDate(100));
+store.dispatch(setEndDate(125));
+store.dispatch(setEndDate());
+store.dispatch(setStartDate());
