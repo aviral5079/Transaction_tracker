209db3a3be10cb4b7b5fa258ac552e819b364ac4
avi5079 <aviral.srivastava2102@gmail.com>
2023-12-14 13:28:53 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
+import { AddTransaction } from "./components/AddTransaction";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseListFilters from "./components/ExpenseListFilters";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <ExpenseListFilters />
-      <ExpenseList />
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,10 +4,14 @@ import filtersReducer from "../reducers/filters";
 
 const configureStore = () => {
   const store = createStore(
-    combineReducers({
-      expenses: expensesReducer,
-      filters: filtersReducer,
-    })
+    combineReducers(
+      {
+        expenses: expensesReducer,
+        filters: filtersReducer,
+      },
+      +window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   );
 
   return store;
